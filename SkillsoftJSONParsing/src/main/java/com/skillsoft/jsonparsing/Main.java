c3e147fcea5507ed13767c394009496457e86f11
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-13 15:48:07 -0600

    Reading and Writing JSON Arrays

--- SkillsoftJSONParsing/src/main/java/com/skillsoft/jsonparsing/Main.java
+++ SkillsoftJSONParsing/src/main/java/com/skillsoft/jsonparsing/Main.java
@@ -1,139 +1,79 @@
 package com.skillsoft.jsonparsing;
 
+import org.json.JSONArray; // class JSONArray
 import org.json.JSONObject; // class JSONObject
 
-import java.io.BufferedReader; // class BufferedReader
-import java.io.FileReader; // class FileReader
 import java.io.FileWriter; // class FileWriter
 import java.io.IOException; // class IOException
 import java.nio.file.Files; // final class Files
 import java.nio.file.Paths; // final class Paths
-import java.util.HashMap; // class HashMap
-import java.util.Map; // interface Map
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String employeeString = new String(Files.readAllBytes(Paths.get("employee.json")));
+        String employeeString = new String(Files.readAllBytes(Paths.get("employees.json")));
 
-        JSONObject employeeJson = new JSONObject(employeeString);
+        JSONArray employeesJson = new JSONArray(employeeString);
 
-        String name = employeeJson.getString("name");
-        double salary = employeeJson.getDouble("salary");
+        for (int i = 0; i < employeesJson.length(); i++) {
 
-        JSONObject contactJson = employeeJson.getJSONObject("contact");
+            JSONObject employeeJson = employeesJson.getJSONObject(i);
 
-        String phone = contactJson.getString("phone");
-        String email = contactJson.getString("email");
+            String name = employeeJson.getString("name");
+            String title = employeeJson.getString("title");
+            double salary = employeeJson.getDouble("salary");
 
-        System.out.format("\n%s, %s, %s, %.1f\n", name, phone, email, salary);
-        //Peter, 212-555-3421, peter@gmail.com, 122000.0
+            System.out.format("\n***\n%s, %s, %.1f\n", name, title, salary);
+            //***
+            //Peter, Vice President, 122000.0
+            //***
+            //Donna, Tech Lead, 108000.0
 
-//        JSONObject employeeJson = new JSONObject();
-//
-//        employeeJson.put("name", "Peter");
-//        employeeJson.put("age", 42);
-//        employeeJson.put("gender", JSONObject.NULL);
-//        employeeJson.put("title", "Vice President");
-//        employeeJson.put("salary", 122000);
-//
-//        JSONObject contactJson = new JSONObject();
-//        contactJson.put("phone", "212-555-3421");
-//        contactJson.put("email", "peter@gmail.com");
-//        contactJson.put("address", "1234, 14th Street, New York, NY-10011");
-//
-//        employeeJson.put("contact", contactJson);
-//
-//        System.out.println(employeeJson.toString(2));
-//        //{
-//        //  "gender": null,
-//        //  "contact": {
-//        //    "address": "1234, 14th Street, New York, NY-10011",
-//        //    "phone": "212-555-3421",
-//        //    "email": "peter@gmail.com"
-//        //  },
-//        //  "name": "Peter",
-//        //  "title": "Vice President",
-//        //  "salary": 122000,
-//        //  "age": 42
-//        //}
-//
-//        try (FileWriter file = new FileWriter("employee.json")) {
-//            employeeJson.write(file, 2, 0);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+            int age = employeeJson.optInt("age");
+            String gender = employeeJson.optString("gender");
 
-//        try (BufferedReader reader = new BufferedReader(new FileReader("employee.json"))) {
-//
-//            StringBuilder stringBuilder = new StringBuilder();
-//
-//            String line = null;
-//            String ls = System.getProperty("line.separator");
-//
-//            while ((line = reader.readLine()) != null) {
-//                stringBuilder.append(line);
-//                stringBuilder.append(ls);
-//            }
-//
-//            JSONObject employeeJson = new JSONObject(stringBuilder.toString());
-//
-//            String name = employeeJson.getString("name");
-//            String title = employeeJson.getString("title");
-//
-//            double salary = employeeJson.getDouble("salary");
-//            int age = employeeJson.getInt("age");
-//
-//            System.out.format("\n%s, %s, %.1f, %d\n", name, title, salary, age);
-//            //Peter, Vice President, 122000.0, 42
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+            System.out.format("%d, %s\n", age, gender);
+            //42,
+            //0,
+        }
 
-//        String employeeString = "{" +
-//                "\"name\":\"Peter\"," +
-//                "\"age\":42," +
-//                "\"gender\":null," +
-//                "\"title\":\"Vice President\"," +
-//                "\"salary\":122000" +
-//                "}";
-//        JSONObject employeeJson = new JSONObject(employeeString);
+//        JSONObject employeeJson1 = new JSONObject();
 //
-//        System.out.println(employeeJson.toString(2));
-//        //{
-//        //  "gender": null,
-//        //  "name": "Peter",
-//        //  "title": "Vice President",
-//        //  "salary": 122000,
-//        //  "age": 42
-//        //}
+//        employeeJson1.put("name", "Peter");
+//        employeeJson1.put("age", 42);
+//        employeeJson1.put("gender", JSONObject.NULL);
+//        employeeJson1.put("title", "Vice President");
+//        employeeJson1.put("salary", 122000);
 //
-//        try (FileWriter file = new FileWriter("employee.json")) {
-//            employeeJson.write(file, 2, 0);
+//        JSONObject employeeJson2 = new JSONObject();
 //
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-//        Map<String, String>  map = new HashMap<>();
+//        employeeJson2.put("name", "Donna");
+//        employeeJson2.put("title", "Tech Lead");
+//        employeeJson2.put("salary", 108000);
 //
-//        map.put("name", "Peter");
-//        map.put("title", "Vice President");
-//        map.put("department", "Sales");
+//        JSONArray employeesArray = new JSONArray();
+//        employeesArray.put(employeeJson1);
+//        employeesArray.put(employeeJson2);
 //
-//        JSONObject employeeJson = new JSONObject(map);
-//
-//        System.out.println(employeeJson.toString(4));
-//        //{
+//        System.out.println(employeesArray.toString(2));
+//        //[
+//        //  {
+//        //    "gender": null,
 //        //    "name": "Peter",
 //        //    "title": "Vice President",
-//        //    "department": "Sales"
-//        //}
+//        //    "salary": 122000,
+//        //    "age": 42
+//        //  },
+//        //  {
+//        //    "name": "Donna",
+//        //    "title": "Tech Lead",
+//        //    "salary": 108000
+//        //  }
+//        //]
 //
-//        try (FileWriter file = new FileWriter("employee.json")) {
-//            employeeJson.write(file, 4, 0);
+//        try (FileWriter file = new FileWriter("employees.json")) {
+//            employeesArray.write(file, 2, 0);
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
