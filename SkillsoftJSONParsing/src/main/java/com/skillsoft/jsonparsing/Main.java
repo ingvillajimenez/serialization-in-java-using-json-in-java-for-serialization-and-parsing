94e2598b509fb39806e5bc8331424db95960787c
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-14 07:42:26 -0600

    Using the CDL Class to Parse Comma-delimited Values

--- SkillsoftJSONParsing/src/main/java/com/skillsoft/jsonparsing/Main.java
+++ SkillsoftJSONParsing/src/main/java/com/skillsoft/jsonparsing/Main.java
@@ -1,62 +1,177 @@
 package com.skillsoft.jsonparsing;
 
-import org.json.JSONObject; // class JSONObject
+import org.json.CDL; // class CDL
 import org.json.JSONArray; // class JSONArray
-
-import java.io.FileWriter; // class FileWriter
-import java.io.IOException; // class IOException
-import java.math.BigInteger; // class BigInteger
-import java.util.ArrayList; // class ArrayList
-import java.util.List; // interface List
+import org.json.JSONTokener; // class JSONTokener
+import org.json.JSONObject; // class JSONObject
 
 public class Main {
 
     public static void main(String[] args) {
 
-        EmployeeBean employeeBean1 = new EmployeeBean(
-                BigInteger.valueOf(1234567), "John Dillard", "Business Analyst", 56000, 28);
+        String commaDelimitedText = "name, age, gender \n" +
+                "Peter, 42, male \n" +
+                "Jane, 22, female \n" +
+                "Ophelia, 25,  \n" +
+                "Bob, 33, male";
 
-        EmployeeBean employeeBean2 = new EmployeeBean();
-        employeeBean2.setId(BigInteger.valueOf(3451230));
-        employeeBean2.setName("Julia Dreyfus");
-        employeeBean2.setTitle("Vice President");
-        employeeBean2.setAge(35);
+        JSONArray array = CDL.toJSONArray(commaDelimitedText);
 
-        EmployeeBean employeeBean3 = new EmployeeBean();
-        employeeBean3.setId(BigInteger.valueOf(5555));
-        employeeBean3.setName("Doris Smith");
+        System.out.println(array.toString(2));
 
-        List<EmployeeBean> list = new ArrayList<>();
-        list.add(employeeBean1);
-        list.add(employeeBean2);
-        list.add(employeeBean3);
-
-        JSONArray jsonArray = new JSONArray(list);
-        System.out.println(jsonArray.toString(2));
         //[
         //  {
-        //    "full_name": "John Dillard",
-        //    "employee_id": 1234567,
-        //    "designation": "Business Analyst"
+        //    "name": "Peter",
+        //    "gender": "male",
+        //    "age": "42"
         //  },
         //  {
-        //    "full_name": "Julia Dreyfus",
-        //    "employee_id": 3451230,
-        //    "designation": "Vice President"
+        //    "name": "Jane",
+        //    "gender": "female",
+        //    "age": "22"
         //  },
         //  {
-        //    "full_name": "Doris Smith",
-        //    "employee_id": 5555
+        //    "name": "Ophelia",
+        //    "gender": "",
+        //    "age": "25"
+        //  },
+        //  {
+        //    "name": "Bob",
+        //    "gender": "male",
+        //    "age": "33"
         //  }
         //]
 
-        try (FileWriter file = new FileWriter("employees.json")) {
-            jsonArray.write(file, 2, 0);
+//        JSONArray headerArray = new JSONArray();
+//
+//        headerArray.put("name");
+//        headerArray.put("age");
+//        headerArray.put("gender");
+//
+//        String commaDelimitedText = "Peter, 42, male \n" +
+//                "Jane, 22, female \n" +
+//                "Ophelia, 25, null \n" +
+//                "Bob, 33, male";
+//
+//        JSONArray array = CDL.toJSONArray(headerArray, commaDelimitedText);
+//
+//        System.out.println(array.toString(2));
+        //[
+        //  {
+        //    "name": "Peter",
+        //    "gender": "male",
+        //    "age": "42"
+        //  },
+        //  {
+        //    "name": "Jane",
+        //    "gender": "female",
+        //    "age": "22"
+        //  },
+        //  {
+        //    "name": "Ophelia",
+        //    "gender": "null",
+        //    "age": "25"
+        //  },
+        //  {
+        //    "name": "Bob",
+        //    "gender": "male",
+        //    "age": "33"
+        //  }
+        //]
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        JSONArray fieldsArray = CDL.rowToJSONArray(new JSONTokener(
+//                "ID, LoanStatus, LoanAmount, Term, CreditScore, AnnualIncome, Home, CreditBalance"));
+//
+//        System.out.println(fieldsArray);
+//        //["ID","LoanStatus","LoanAmount","Term","CreditScore","AnnualIncome","Home","CreditBalance"]
+//
+//        JSONObject loanJson1 = CDL.rowToJSONObject(fieldsArray, new JSONTokener(
+//                "c89ed2cc, Fully Paid, 313544, Short Term, 716.0, 1353940.0, Home Mortgage, 195985"));
+//
+//        JSONObject loanJson2 = CDL.rowToJSONObject(fieldsArray, new JSONTokener(
+//                "dbf1d5da, Charged Off, 342518, Long Term, 616.0, 1404195.0, Own Home, 621509"));
+//        JSONObject loanJson3 = CDL.rowToJSONObject(fieldsArray, new JSONTokener(
+//                "bcbf02c0, Fully Paid, 176880, Short Term, 724.0, 668325.0, Rent, 342532"));
+//
+//        JSONObject loansJson = new JSONObject();
+//        loansJson.append("loans", loanJson1);
+//        loansJson.append("loans", loanJson2);
+//        loansJson.append("loans", loanJson3);
+//
+//        System.out.println(loansJson.toString(2));
+//        //{"loans": [
+//        //  {
+//        //    "LoanAmount": "313544",
+//        //    "CreditBalance": "195985",
+//        //    "LoanStatus": "Fully Paid",
+//        //    "ID": "c89ed2cc",
+//        //    "Term": "Short Term",
+//        //    "CreditScore": "716.0",
+//        //    "Home": "Home Mortgage",
+//        //    "AnnualIncome": "1353940.0"
+//        //  },
+//        //  {
+//        //    "LoanAmount": "342518",
+//        //    "CreditBalance": "621509",
+//        //    "LoanStatus": "Charged Off",
+//        //    "ID": "dbf1d5da",
+//        //    "Term": "Long Term",
+//        //    "CreditScore": "616.0",
+//        //    "Home": "Own Home",
+//        //    "AnnualIncome": "1404195.0"
+//        //  },
+//        //  {
+//        //    "LoanAmount": "176880",
+//        //    "CreditBalance": "342532",
+//        //    "LoanStatus": "Fully Paid",
+//        //    "ID": "bcbf02c0",
+//        //    "Term": "Short Term",
+//        //    "CreditScore": "724.0",
+//        //    "Home": "Rent",
+//        //    "AnnualIncome": "668325.0"
+//        //  }
+//        //]}
 
+//        JSONArray fieldsArray = CDL.rowToJSONArray(new JSONTokener(
+//                "ID, LoanStatus, LoanAmount, Term, CreditScore, AnnualIncome, Home, CreditBalance"));
+//
+//        System.out.println(fieldsArray);
+//        //["ID","LoanStatus","LoanAmount","Term","CreditScore","AnnualIncome","Home","CreditBalance"]
+//
+//        JSONObject loadJson = CDL.rowToJSONObject(fieldsArray, new JSONTokener(
+//                "c89ed2cc, Fully Paid, 313544, Short Term, 716.0, 1353940.0, Home Mortgage, 195985"));
+//
+//        System.out.println(loadJson.toString(2));
+//        //{
+//        //  "LoanAmount": "313544",
+//        //  "CreditBalance": "195985",
+//        //  "LoanStatus": "Fully Paid",
+//        //  "ID": "c89ed2cc",
+//        //  "Term": "Short Term",
+//        //  "CreditScore": "716.0",
+//        //  "Home": "Home Mortgage",
+//        //  "AnnualIncome": "1353940.0"
+//        //}
+
+//        JSONObject studentsJson = new JSONObject();
+//
+//        JSONArray namesArray = CDL.rowToJSONArray(new JSONTokener("Peter, Jane, Bob, Nora, Hannah"));
+//
+//        studentsJson.put("names", namesArray);
+//
+//        System.out.println(studentsJson.toString(2));
+//        //{"names": [
+//        //  "Peter",
+//        //  "Jane",
+//        //  "Bob",
+//        //  "Nora",
+//        //  "Hannah"
+//        //]}
+
+//        JSONArray namesArray = CDL.rowToJSONArray(new JSONTokener("Peter, Jane, Bob, Nora, Hannah"));
+//
+//        System.out.println(namesArray);
+//        //["Peter","Jane","Bob","Nora","Hannah"]
     }
 }
 
@@ -77,3 +192,6 @@ public class Main {
 
 // Reflection on Getters
 // Any no-argument method which starts with "get" or "is" followed by an uppercase letter
+
+// CDL (Comma Delimited List)
+// Convert comma-delimited values to a JSON array and vice-versa
